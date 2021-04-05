1af5adaf7e5914c461103732846b6f91b09fd8a3
Teh Yee Xin <73182761+tehyeexin@users.noreply.github.com>
2021-04-05 21:21:35 +0800

    Add files via upload

--- Exercise 2.2/MainSmartphones.java
+++ Exercise 2.2/MainSmartphones.java
@@ -12,45 +12,45 @@ public class MainSmartphones {
 		
 		System.out.println("- APPLE SPECIFICATION -");
 		System.out.println();
-		Apple.phoneSpec("Apple", "iPhone 12 Pro Max", "Pacific blue", 6.7, 6999, 'Y');
+		Apple.phoneSpec("Apple", "iPhone 12 Pro Max", "Pacific blue", 6.7, 228, 4899, 'Y');
 		System.out.println();
-		Apple.totalprice(6999, 2, "iPhone 12 Pro Max");
-		Apple.PhoneCall("iPhone 12 Pro Max");
-		Apple.SendMessage("iPhone 12 Pro Max");
-		Apple.PlayingGame("iPhone 12 Pro Max");
+		Apple.totalPrice(6999, 2, "iPhone 12 Pro Max");
+		Apple.chargingTime(3.687, 3, "iPhone 12 Pro Max");
+		Apple.dimensions(160.8, 78.1, 7.4, "iPhone 12 Pro Max");
+		Apple.weight(228, 2, "iPhone 12 Pro Max");
 		System.out.println("_______________________________________________________________________");
 		System.out.println();
 		
 		System.out.println("- SAMSUNG SPECIFICATION -");
 		System.out.println();
-		Samsung.phoneSpec("Samsung", "Samsung Galazy Z Flip", "Mirror purple", 6.7, 3999, 'Y');
+		Samsung.phoneSpec("Samsung", "Samsung Galazy Z Flip", "Mirror purple", 6.7, 183, 3999, 'Y');
 		System.out.println();
-		Samsung.totalprice(3999, 3, "Samsung Galazy Z Flip");
-		Samsung.PhoneCall("Samsung Galazy Z Flip");
-		Samsung.SendMessage("Samsung Galazy Z Flip");
-		Samsung.PlayingGame("Samsung Galazy Z Flip");
+		Samsung.totalPrice(3999, 3, "Samsung Galazy Z Flip");
+		Samsung.chargingTime(3.3, 3, "Samsung Galazy Z Flip");
+		Samsung.dimensions(167.3, 73.6, 7.2, "Samsung Galazy Z Flip");
+		Samsung.weight(183, 3, "Samsung Galazy Z Flip");
 		System.out.println("_______________________________________________________________________");
 		System.out.println();
 		
 		System.out.println("- HUAWEI SPECIFICATION -");
 		System.out.println();
-		Huawei.phoneSpec("Huawei", "Huawei Mate 40 Pro", "Mystic silver", 6.76, 6999, 'Y');
+		Huawei.phoneSpec("Huawei", "Huawei Mate 40 Pro", "Mystic silver", 6.76, 212, 6999, 'Y');
 		System.out.println();
-		Huawei.totalprice(6999, 4, "Huawei Mate 40 Pro");
-		Huawei.PhoneCall("Huawei Mate 40 Pro");
-		Huawei.SendMessage("Huawei Mate 40 Pro");
-		Huawei.PlayingGame("Huawei Mate 40 Pro");
+		Huawei.totalPrice(6999, 4, "Huawei Mate 40 Pro");
+		Huawei.chargingTime(4.4, 4.5, "Huawei Mate 40 Pro");
+		Huawei.dimensions(162.9, 75.5, 9.1, "Huawei Mate 40 Pro");
+		Apple.weight(212, 4, "Huawei Mate 40 Pro");
 		System.out.println("_______________________________________________________________________");
 		System.out.println();
 		
 		System.out.println("- OPPO SPECIFICATION -");
 		System.out.println();
-		Oppo.phoneSpec("Oppo", "Oppo Find X3 Pro", "Gloss black", 6.7, 4299, 'Y');
+		Oppo.phoneSpec("Oppo", "Oppo Find X3 Pro", "Gloss black", 6.7, 193, 4299, 'Y');
 		System.out.println();
-		Oppo.totalprice(4299, 5, "Oppo Find X3 Pro");
-		Oppo.PhoneCall("Oppo Find X3 Pro");
-		Oppo.SendMessage("Oppo Find X3 Pro");
-		Oppo.PlayingGame("Oppo Find X3 Pro");
+		Oppo.totalPrice(4299, 5, "Oppo Find X3 Pro");
+		Oppo.chargingTime(4.5, 4, "Oppo Find X3 Pro");
+		Oppo.dimensions(163.6, 74, 8.3, "Oppo Find X3 Pro");
+		Apple.weight(193, 5, "Oppo Find X3 Pro");
 		System.out.println("_______________________________________________________________________");
 		System.out.println();
 		
--- Exercise 2.2/Smartphones.java
+++ Exercise 2.2/Smartphones.java
@@ -6,33 +6,38 @@ public class Smartphones {
 	String model;
 	String color;
 	double screenSize;
+	int weight;
 	int price;
 	char fingerprint;
 	
-	void phoneSpec(String b, String m, String c, double s, int p, char f) {
+	void phoneSpec(String b, String m, String c, double s, int w, int p, char f) {
 		System.out.println("Brand       : " + b +
 							"\nModel       : " + m +
 							"\nColor       : " + c +
 							"\nScreen size : " + s + " inches" +
+							"\nWeight      : " + w + "g" +
 							"\nPrice       : RM" + p +
 							"\nFingerprint : " + f);
 	}
 	
-	void totalprice(int p, int q, String m) {
-		int total = p*q;
-		System.out.println("The total price of " + q + " " + m + " is RM" + total + ".");
+	void totalPrice(int p, int quantity, String m) {
+		int totalPrice = p*quantity;
+		System.out.println("The total price of " + quantity + " " + m + " is RM" + totalPrice + ".");
 	}
 	
-	void PhoneCall(String m) {
-		System.out.println(m + " can make phone calls with other people.");
+	void chargingTime(double Ah, double A, String m) {
+		double chargingTime = Ah/A; // battery capacity (Ah) / charging current (A)
+		System.out.printf("The charging time of " + m + " is %.2f", chargingTime);
+		System.out.println(" hour.");
 	}
 
-	void SendMessage(String m) {
-		System.out.println(m + " have the function of sending messages to others.");
+	void dimensions(double height, double width, double depth, String m) {
+		System.out.println("The dimensions of " + m + " is " + height + "mm X "+ width + "mm X "+ depth + "mm.");
 	}
 
-	void PlayingGame(String m) {
-		System.out.println(m + " can play various games.");
+	void weight(int w, int quantity, String m) {
+		int totalWeight = w*quantity;
+		System.out.println("The total weight of " + quantity + " " + m + " is " + totalWeight + "g.");
 	}
 	
 	
